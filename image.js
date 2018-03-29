61077c292ba73a6906821b40d4aed109e8e17f81
mstrieleman <mstrieleman@gmail.com>
2018-03-29 13:59:28 -0700

    Revised Image.js.

--- image.js
+++ image.js
@@ -1,75 +1,81 @@
 import React, { Component } from 'react';
 
-//THIS DOES NOTHING YET  ¯\_(ツ)_/¯
-class ImageSearchData extends Component {
-  componentDidMount() {}
-}
+const formData = new FormData(event);
+const data = {
+  url: 'https://api.flickr.com/services/rest/?',
+  method: 'method=flickr.photos.search&',
+  apiKey: 'api_key=e03c0952f82752553d79c8f7a18523f0&',
+  tags: 'tags=' + formData.get('search'),
+  output: '&format=json&nojsoncallback=1'
+};
+const request = data.url + data.method + data.apiKey + data.tags + data.output;
 
-//THIS DOES EVERYTHING  ¯\_(ツ)_/¯
-function handleSubmit(event) {
-  event.preventDefault();
-  const formData = new FormData(event.target);
-  //CONSTRUCT API URL
-  const data = {
-    url: 'https://api.flickr.com/services/rest/?',
-    method: 'method=flickr.photos.search&',
-    apiKey: 'api_key=e03c0952f82752553d79c8f7a18523f0&',
-    tags: 'tags=' + formData.get('search'),
-    output: '&format=json&nojsoncallback=1'
-  };
-  //STORE API URL
-  const request =
-    data.url + data.method + data.apiKey + data.tags + data.output;
+export default class ImageSearch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: []
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-  //GET DATA WITH URL
-  fetch(request)
-    .then(data => {
-      return data.json();
-    })
-    //DO STUFF WITH DATA
-    .then(data => {
-      console.log(data);
-      //LOOP THROUGH AND CONSTRUCT URLS FOR IMAGES
-      for (let i = 0; i < data.photos.photo.length; i++) {
-        let farm = data.photos.photo[i].farm;
-        let server = data.photos.photo[i].server;
-        let id = data.photos.photo[i].id;
-        let secret = data.photos.photo[i].secret;
-        let imgSource =
-          'http://farm' +
-          farm +
-          '.static.flickr.com/' +
-          server +
-          '/' +
-          id +
-          '_' +
-          secret +
-          '.jpg';
-        console.log(imgSource);
-      }
-    });
-  event.target.reset();
-}
+  handleSubmit(event) {
+    event.preventDefault();
+    fetch(request)
+      .then(data => {
+        if (!data.ok) {
+          throw Error('Request Failed!');
+        }
+        return data.json();
+      })
+      .then(data => {
+        for (let i = 0; i < data.photos.photo.length; i++) {
+          let farm = data.photos.photo[i].farm;
+          let server = data.photos.photo[i].server;
+          let id = data.photos.photo[i].id;
+          let secret = data.photos.photo[i].secret;
+          let imgSource =
+            'http://farm' +
+            farm +
+            '.static.flickr.com/' +
+            server +
+            '/' +
+            id +
+            '_' +
+            secret +
+            '.jpg';
+          this.state.images.push(imgSource);
+          console.log(this.state.images);
+          //MAYBE YOU SHOULD USE SET STATE
+        }
+      });
+    event.target.reset();
+  }
 
-//~ TYPE THE STUFF ~ PRESS THE BUTTON ~ DO THE THING ~
-export default function ImageSearchUI() {
-  return (
-    <form onSubmit={handleSubmit}>
-      <div className="form-group">
-        <label htmlFor="search" />
-        <input
-          name="search"
-          placeholder="Search images here!"
-          id="search"
-          type="text"
-          className="form-search"
-        />
+  render() {
+    return (
+      <div className="images">
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="search" />
+            <input
+              name="search"
+              placeholder="Search images here!"
+              id="search"
+              type="text"
+              className="form-search"
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn main-btn">
+              Go!
+            </button>
+          </div>
+        </form>
+        <div>
+          <img src={this.state.images[0]} />
+        </div>
       </div>
-      <div className="form-group">
-        <button type="submit" className="btn main-btn">
-          Go!
-        </button>
-      </div>
-    </form>
-  );
+    );
+  }
 }
