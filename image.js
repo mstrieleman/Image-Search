56f81e573c5dd22b9d86f78f4e2ad1a4030c1d36
mstrieleman <mstrieleman@gmail.com>
2018-03-29 10:20:45 -0700

    Updated for issue #1 task - Returned data from flickr builds into an image location/source.

--- image.js
+++ image.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
+//THIS DOES NOTHING YET  ¯\_(ツ)_/¯
 class ImageSearchData extends Component {
   componentDidMount() {}
 }
 
+//THIS DOES EVERYTHING  ¯\_(ツ)_/¯
 function handleSubmit(event) {
   event.preventDefault();
   const formData = new FormData(event.target);
+  //CONSTRUCT API URL
   const data = {
     url: 'https://api.flickr.com/services/rest/?',
     method: 'method=flickr.photos.search&',
@@ -14,22 +17,41 @@ function handleSubmit(event) {
     tags: 'tags=' + formData.get('search'),
     output: '&format=json&nojsoncallback=1'
   };
+  //STORE API URL
   const request =
     data.url + data.method + data.apiKey + data.tags + data.output;
 
+  //GET DATA WITH URL
   fetch(request)
-    .then(response => {
-      return response.json();
+    .then(data => {
+      return data.json();
     })
-    .then(response => {
-      console.log(response);
+    //DO STUFF WITH DATA
+    .then(data => {
+      console.log(data);
+      //LOOP THROUGH AND CONSTRUCT URLS FOR IMAGES
+      for (let i = 0; i < data.photos.photo.length; i++) {
+        let farm = data.photos.photo[i].farm;
+        let server = data.photos.photo[i].server;
+        let id = data.photos.photo[i].id;
+        let secret = data.photos.photo[i].secret;
+        let imgSource =
+          'http://farm' +
+          farm +
+          '.static.flickr.com/' +
+          server +
+          '/' +
+          id +
+          '_' +
+          secret +
+          '.jpg';
+        console.log(imgSource);
+      }
     });
-
   event.target.reset();
 }
 
-function handleChange({ target }) {}
-
+//~ TYPE THE STUFF ~ PRESS THE BUTTON ~ DO THE THING ~
 export default function ImageSearchUI() {
   return (
     <form onSubmit={handleSubmit}>
@@ -41,7 +63,6 @@ export default function ImageSearchUI() {
           id="search"
           type="text"
           className="form-search"
-          onChange={handleChange}
         />
       </div>
       <div className="form-group">
