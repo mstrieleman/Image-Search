b7fa14e3cbbbda3dd30d52522a1b77bea7fc9ad8
mstrieleman <mstrieleman@gmail.com>
2018-04-06 12:39:37 -0700

    Revised index.js.

--- index.js
+++ index.js
@@ -2,9 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ImageSearch from './image';
 
-// MDB Lightbox Init
-$(function() {
-  $('#mdb-lightbox-ui').load('mdb-addons/mdb-lightbox-ui.html');
-});
-
 ReactDOM.render(<ImageSearch />, document.querySelector('#app'));
